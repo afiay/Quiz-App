e8545d1bd894c534118d024867516247f54d9232
afiay <59549940+afiay@users.noreply.github.com>
2022-06-09 19:55:06 +0200

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,58 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    number: 1,
+    question: "What is the capital of Finland?",
+    answer: "Helsinki",
+    options: [
+      "Stockholm",
+      "Kopenhagen",
+      "Helsinki",
+      "Oslo"
+    ]
+  },
+    {
+    number: 2,
+    question: "How many minutes in a game of rugby league?",
+    answer: "80 minutes",
+    options: [
+      "60 minutes",
+      "70 minutes",
+      "80 minutes",
+      "90 minutes"
+    ]
+  },
+    {
+    number: 3,
+    question: "What fruit takes the scientific name Mangifera indica?",
+    answer: "Mango",
+    options: [
+      "Watermelon",
+      "Kiwi",
+      "Mango",
+      "Persimmon"
+    ]
+  },
+    {
+    number: 4,
+    question: "What month was Prince George born?",
+    answer: "July",
+    options: [
+      "May",
+      "Juin",
+      "July",
+      "August"
+    ]
+  },
+    {
+    number: 5,
+    question: "What grain is the Japanese spirit Sake made from?",
+    answer: "Rice",
+    options: [
+      "Shiraz",
+      "Rice",
+      "Nebbiolo",
+      "Mataro"
+    ]
+  },
+];
